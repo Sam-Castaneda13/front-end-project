f4f9697173becd3df959ebc17c480d9d58b8b129
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-08-19 17:29:12 +0000

    Can load the image into the page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 'use strict';
-// Fetch the data from Monster Hunter
-const monstersData = async () => {
-  const response = await fetch('https://mhw-db.com/monsters');
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-const monsters = monstersData;
-console.log(monstersData());
+const $row = document.querySelector('.row');
+async function makeMonsters() {
+  if (!$row) throw new Error('Could not load row');
+  for (let i = 1; i < 6; i++) {
+    try {
+      const response = await fetch(`https://mhw-db.com/monsters/${i}`);
+      if (!response.ok) {
+        throw new Error('This is not ok');
+      }
+      const monster = await response.json();
+      console.log(monster);
+      const newPage = renderMonster(monster);
+      $row.append(newPage);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+}
+makeMonsters();
+function renderMonster(object) {
+  /* <div class="row">
+        <div class="column-one-third">
+          <img src='link' class="monster-img"/>
+          <div class="monster-name">
+        </div>
+        </div>
+      This times 3
+    */
+  // Starting Dom Creation
+  const $oneThird = document.createElement('div');
+  $oneThird.className = 'column-one-third';
+  const $monImg = document.createElement('img');
+  $monImg.className = 'monster-img';
+  $monImg.setAttribute('src', `images/icons/${object.name}.webp`);
+  const $monName = document.createElement('div');
+  $monName.className = 'monster-name';
+  $monName.textContent = object.name;
+  $oneThird.append($monImg);
+  $oneThird.append($monName);
+  return $oneThird;
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,56 @@
-// Monster Interface
+// interface for Monster
+interface Monster {
+  id: number;
+  name: string;
+  type: string;
+  description: string;
+  locations: [];
+}
+
+const $row = document.querySelector('.row');
+
+async function makeMonsters(): Promise<void> {
+  if (!$row) throw new Error('Could not load row');
+  for (let i = 1; i < 6; i++) {
+    try {
+      const response = await fetch(`https://mhw-db.com/monsters/${i}`);
+      if (!response.ok) {
+        throw new Error('This is not ok');
+      }
+      const monster = (await response.json()) as Monster;
+      console.log(monster);
+      const newPage = renderMonster(monster);
+      $row.append(newPage);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+}
+makeMonsters();
+
+function renderMonster(object: Monster): HTMLDivElement {
+  /* <div class="row">
+      <div class="column-one-third">
+        <img src='link' class="monster-img"/>
+        <div class="monster-name">
+      </div>
+      </div>
+    This times 3
+  */
+
+  // Starting Dom Creation
+  const $oneThird = document.createElement('div');
+  $oneThird.className = 'column-one-third';
+
+  const $monImg = document.createElement('img');
+  $monImg.className = 'monster-img';
+  $monImg.setAttribute('src', `images/icons/${object.name}.webp`);
+
+  const $monName = document.createElement('div');
+  $monName.className = 'monster-name';
+  $monName.textContent = object.name;
+
+  $oneThird.append($monImg);
+  $oneThird.append($monName);
+  return $oneThird;
+}
